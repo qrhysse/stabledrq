460153c66e59b93973c32c93c338ba0489969cb6
qrhysse <chrisharrison014@gmail.com>
2016-07-10 19:39:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,33 +3,61 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
+var botPatience = 0;
+
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegexsts = /Dr. Q, status(!|.)?/; 
-      botRegexBio = /(F|f)rom a biological/;
-      botRegexWee = /(KUN|kun)/;
-  if(request.text && botRegexsts.test(request.text)) {
+    var request = JSON.parse(this.req.chunks[0]),
+        botRegexsts = /Dr. Q, status(!|.)?/; 
+        botRegexpat = /^\/patience/;
+        botRegexBio = /(F|f)rom a biological/;
+        botRegexWee = /(KUN|kun)/;
+        botRegexBTW = /Get back to work, Dr. Q/;
+    if(request.text && botRegexsts.test(request.text) && botPatience < 3) {
+      this.res.writeHead(200);
+      postMessage(cool());
+      botPatience++;
+      this.res.end();
+    } 
+    
+    else if(request.text && botRegexpat.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(cool());
+    postMessage(botPatience);
     this.res.end();
-  } 
-  else if(request.text && botRegexBio.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://google.com");
-    this.res.end();
-  } 
-  
-  else if(request.text && botRegexWee.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("Weeaboo");
-    this.res.end();
-  } 
-  
-  else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+    }
+    
+    else if(request.text && botRegexBio.test(request.text) && botPatience < 3) {
+      this.res.writeHead(200);
+      postMessage("http://google.com");
+      botPatience++;
+      this.res.end();
+    } 
+    
+    
+    else if(request.text && botRegexWee.test(request.text) && botPatience < 3) {
+      this.res.writeHead(200);
+      postMessage("Weeaboo");
+      botPatience++;
+      this.res.end();
+    } 
+    
+    else if(request.text && botRegexBTW.test(request.text) && botPatience < 3) {
+      this.res.writeHead(200);
+      postMessage("okay");
+      botPatience = 0;
+      this.res.end();
+    } 
+    
+    else if(botPatience >= 3 && botPatience <= 5){
+      this.res.writeHead(200);
+      postMessage(""d[-_-]b "");
+      this.res.end();
+    }
+    
+    else {
+      console.log("don't care");
+      this.res.writeHead(200);
+      this.res.end();
+    }
 }
 
 function postMessage(response) {
