b1b4ee7f75cbc450be955c26c4139816757d3523
qrhysse <chrisharrison014@gmail.com>
2016-07-13 18:30:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,19 +3,82 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
+// Non-breaking spaces save the day
+function spaceCalc(sbheader, header, obj) {
+  var num = sbheader.length - (String(obj).length + header.length);
+  return Array(num+1).join('\u00A0');
+}
+
+function getScoreboard(lols, darns) {
+    console.log('getScoreboard called');
+    var scoreboardHead = "-------------SCOREBOARD-------------";
+    var totalLols = "TOTAL LOLS:";
+    var totalDarns = "TOTAL DARNS:";
+    var lolspaces = spaceCalc(scoreboardHead, totalLols, lols);
+    var darnspaces = spaceCalc(scoreboardHead, totalDarns, darns);
+    var lolLine = totalLols + lolspaces + lols;
+    var darnLine = totalDarns + darnspaces + darns;
+    var scoreboard = "-------------SCOREBOARD-------------\n\n" + lolLine + "\n" + darnLine + "\n\n---------Since July 13 2016---------";
+    console.log('Scoreboard is: ', scoreboard);
+    return scoreboard;
+}
+
+function scoreboard(forParse, resp) {
+  var lolTrigger = /(lol|\blol)/ig;
+  var darnTrigger = /(darn|\bdarn)/ig;
+  var botRegexScoreboard = /\/scoreboard/i;
+  var request = require('request');
+  var jsonObj, lolCount, darnCount, returnval = 0;
+  var count = 0;
+
+  request('https://api.myjson.com/bins/4xupz', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var jsonObj = JSON.parse(body);
+      var lolCount = jsonObj.lols;
+      var darnCount = jsonObj.darns;
+      if( forParse && botRegexScoreboard.test(forParse)) {
+        returnval = getScoreboard(lolCount, darnCount);
+        resp.res.writeHead(200);
+        postMessage(returnval, false);
+        resp.res.end();
+      } else {
+        if( lolTrigger.test(forParse)) {
+          count = (forParse.match(lolTrigger) || []).length;
+          lolCount += count;
+        }
+        if( darnTrigger.test(forParse)) {
+          count = (forParse.match(darnTrigger) || []).length;
+          darnCount += count;
+        }
+        request({ url: 'https://api.myjson.com/bins/4xupz', method: 'PUT', json: {lols: lolCount, darns: darnCount}});
+        returnval = 0;
+      }
+    } else {
+      returnval = 0;
+    }
+  });
+}
+
 function respond() { 
   var request = JSON.parse(this.req.chunks[0]);
   var botRegexsts = /Dr. Q, status(!|.)?/i;
   var botRegexBio = /from a biological/i;
   var botRegexWee = /(-|\s[^a-z]?)kun/i;
   var botRegexDad = /(^dad$|\bdad[^a-z]?)/i;
-  var botRegexRip = /(^r\.?i\.?p\.?$|\br\.?i\.?p\.?[^a-z]?)/i;
+  var botRegexRip = /(^r\.?i\.?p\.?$|\sr\.?i\.?p\.?[^a-z]?)/i;
   var botRegexAlex = /(^actually$|\bactually[^a-z]?)/i;
   var botRegexSandwich = /(^sandwich$|\bsandwich[^a-z]?)/i;
+  var botRegexSkeleton= /(^skeletons?$|\bskeletons?[^a-z]?)/i;
+  var sbPost;
+  // var lolTrigger = /(lol|\blol)/ig;
+  // var darnTrigger = /(darn|\bdarn)/ig;
+  // var botRegexScoreboard = /\/scoreboard/i;
   //var botRegexDadJoke = /(\bI'?\s*a?m\b)/g; // I am, I'm, Im, or Iam
   var botRegexDadJoke = /\bi'?m\s+/i;
   var botRegexThbby = /\?\s*$/i;
-      
+  // var loldarnCount = require('./ld.json');
+  
+  // console.log(loldarnCount);
   console.log(request);    
   
   if( request.name !== "Dr. Q" ) {
@@ -24,10 +87,12 @@ function respond() {
       postMessage(cool(), false);
       this.res.end();
     }
+
+    scoreboard( request.text, this );
     
     if(request.text && botRegexBio.test(request.text)) {
       var link = request.text;
-      link = link.replace(/from a biological (perspective|standpoint),?/i, "")
+      link = link.replace(/from a biological (perspective|standpoint),?/i, "");
       link = link.replace(/ /g, "+");
       link = link.replace("%", "%25");
 
@@ -102,6 +167,30 @@ function respond() {
       this.res.writeHead(200);
       postMessage("https://s31.postimg.org/pjuh7qfxn/RIP.jpg", false);
       this.res.end();
+    }
+
+    if( request.text && botRegexSkeleton.test(request.text)) {
+      console.log("Skeleton activated.");
+      this.res.writeHead(200);
+      postMessage("Did somebody say skeleton?", false);
+      this.res.end();
+      
+      // this.res.writeHead(200);
+      // postMessage("Did somebody say skeleton?", false);
+      // this.res.end();
+
+      // setTimeout(function(thing){
+      // thing.res.writeHead(200);
+      // postMessage("https://s31.postimg.org/gzpyios4b/Mrbones1.png", false);
+      // thing.res.end();
+      // }(this), 10000);
+      
+      // setTimeout(function(thing){
+      // thing.res.writeHead(200);
+      // postMessage("https://s32.postimg.org/gen34js5x/Mrbones1_2.png", false);
+      // thing.res.end();
+      // }(this), 200000);
+      
     }
   }
 }
